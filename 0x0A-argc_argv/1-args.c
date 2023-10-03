51c95bb8a0a4191b545a20b4e1cc875cd3a83572
ahmedmohamed2210 <halawaahmed507@gmail.com>
2023-10-04 00:06:33 +0300

    print num of arg

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print the number of argument
+ * @argc:int
+ * @argv:char
+ * Return: 0
+ */
+
+int main(int argc,char *argv[])
+{
+	printf("%d\n",argc);
+	return 0;
+}
