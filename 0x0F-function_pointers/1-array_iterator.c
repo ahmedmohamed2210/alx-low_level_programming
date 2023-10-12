c107fe3e455a507152c4f6f668dcf88920e062ad
ahmedmohamed2210 <halawaahmed507@gmail.com>
2023-10-12 04:49:47 +0300

    applay a function to an array ele

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,10 +2,10 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - execute function in each of 
- * an array element
- * @array: pointer to array
- * @size: size of an array
+ * array_iterator - executes a function given as a parameter
+ * on each element of an array
+ * @array: array to iterate over
+ * @size: size of the array
  * @action: pointer to function used
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
