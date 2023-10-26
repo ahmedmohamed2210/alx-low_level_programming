4e1de05cab2739d274f1e196ef9bc372c9e602bd
ahmedmohamed2210 <halawaahmed507@gmail.com>
2023-10-26 20:49:10 +0300

    c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,13 +15,13 @@ unsigned int binary_to_uint(const char *b)
 
 	for (len = 0; b[len]; len++)
 	{
-		if (b[len] != "0" && b[len] != "1")
+		if (b[len] != '0' && b[len] != '1')
 			return (0);
 	}
 
 	for (power = 1, total = 0, len--; len >= 0; len--, power *= 2)
 	{
-		if (b[len] == "1")
+		if (b[len] == '1')
 			total += power;
 	}
 
